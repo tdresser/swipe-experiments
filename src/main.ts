92a39dd5f6cd94308589b44a553e3566af5a386f
Tim Dresser <tdresser@gmail.com>
2024-02-12 11:35:51 -0500

    Should we use lengths?

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import './style.css'
+
+export function fail(): never {
+  throw new Error("missing element");
+}
+let square = document.getElementById("square") ?? fail();
+let debug = document.getElementById("debug") ?? fail();
+
+const props = ["sX", "sY", "sDist", "XsizeMultiplier", "YsizeMultiplier"];
+
+function update() {
+  window.requestAnimationFrame(update);
+  let s = "";
+  for (let prop of props) {
+    s += prop + " : " + getComputedStyle(square).getPropertyValue("--" + prop) + "</br>";
+  }
+  debug.innerHTML = s;
+}
+
+update();
+
