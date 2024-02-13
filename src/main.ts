cbb10fc9e20823a792fdad24c58313a1fc0ebfd0
Tim Dresser <tdresser@gmail.com>
2024-02-13 11:25:45 -0500

    Using initial pointer position

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,9 @@ const props = ["sX", "sY", "sDist",
 ];
 
 function setProperty(name: string, value: number) {
-  props.push(name);
+  if (!props.includes(name)) {
+    props.push(name);
+  }
   square.style.setProperty('--' + name, value + "");
 }
 
@@ -29,6 +31,18 @@ function update() {
   }
   debug.innerHTML = s;
 }
+
+// Listen for firstPointerMove to exclude touch slop (the distance the pointer moves before we call this a scroll).
+function firstPointerMoveListener(e:PointerEvent) {
+  scroll_container.removeEventListener("pointermove", firstPointerMoveListener);
+  // Relative to the middle of the viewport.
+  setProperty("pointerDownX", e.clientX - window.innerWidth / 2);
+  setProperty("pointerDownY", e.clientY - window.innerHeight / 2);
+}
+
+scroll_container.addEventListener("pointerdown", (e:Event) => {
+  scroll_container.addEventListener("pointermove", firstPointerMoveListener);
+});
 
 update();
 
